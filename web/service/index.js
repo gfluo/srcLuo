291f3d91521a67689d3ee926ebb77bbf54f44c53
luogf <zoroshow@outlook.com>
2016-12-10 19:01:37 +0800

    使用express框架

--- web/service/index.js
+++ web/service/index.js
@@ -1,8 +1,5 @@
-var http = require('http');
 var express = require('express');
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('hello world');
-}).listen(9999);
-///输出
-console.log('Server runnning at 9999');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(9999);
