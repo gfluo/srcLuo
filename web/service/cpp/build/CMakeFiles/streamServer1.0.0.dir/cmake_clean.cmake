6450db7b61f904246e04d340c0e8dca19865722d
luogf <zoroshow@outlook.com>
2016-12-12 10:37:48 +0800

    添加c++模块

---
+++ web/service/cpp/build/CMakeFiles/streamServer1.0.0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/streamServer1.0.0.dir/main.cpp.o"
+  "bin/streamServer1.0.0.pdb"
+  "bin/streamServer1.0.0"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/streamServer1.0.0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
