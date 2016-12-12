b469953f0b354e8371bb42221052625cc81c6aec
luogf <zoroshow@outlook.com>
2016-12-12 10:40:57 +0800

    添加main

---
+++ web/service/cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv){
+  char* map = nullptr; 
+
+  string msg = "网页推流\n";
+  cout << msg << endl;
+  return 0;
+}
